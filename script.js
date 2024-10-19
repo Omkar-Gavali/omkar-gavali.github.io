ef7ceef2ed2d92cb60666b819f81b78de4100780
omkar <Omkar.Gavali@Student.HTW-Berlin.de>
2024-10-19 23:46:12 +0200

    dd

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 particlesJS.load('particles-js', 'particles-config.json', function() {
     console.log('particles.js loaded - callback');
-}); 
+});
 
 window.addEventListener('scroll', function() {
     const header = document.querySelector('header');
@@ -9,4 +9,13 @@ window.addEventListener('scroll', function() {
     } else {
         header.style.backgroundColor = '#333';
     }
+});
+
+// Load Lottie animation
+lottie.loadAnimation({
+    container: document.getElementById('lottie-animation'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets10.lottiefiles.com/packages/lf20_9x9z6z.json' // the path to the animation json
 });
