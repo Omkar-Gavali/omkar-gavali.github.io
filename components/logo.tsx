be2821e958bf3e009d67c0f51748e0752e64dee5
omkar <Omkar.Gavali@Student.HTW-Berlin.de>
2024-11-22 23:12:53 +0100

    done editing

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,52 +1,120 @@
 export default function Logo() {
   return (
     <svg
-      id="logo-gradient"
       xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
-      viewBox="0 0 600 600"
+      viewBox="0 0 200 200"
       width="100px"
-      fill="url(#linear-gradient) #009688"
+      height="100px"
     >
       <defs>
+        {/* Gradient for the letters */}
         <linearGradient
-          id="linear-gradient"
-          x1="435.39"
-          y1="490.86"
-          x2="84.52"
-          y2="139.99"
+          id="gradient"
+          x1="0%"
+          y1="0%"
+          x2="100%"
+          y2="100%"
           gradientUnits="userSpaceOnUse"
         >
-          <stop offset="0" stopColor="transparent">
-            <animate
-              attributeName="stop-color"
-              values="transparent; #004d40"
-              begin="2.5s"
-              dur="0.1s"
-              repeatCount="none"
-              fill="freeze"
-            ></animate>
-          </stop>
-          <stop offset="1" stopColor="transparent">
-            <animate
-              attributeName="stop-color"
-              values="transparent; #26a69a"
-              begin="2.5s"
-              dur="0.1s"
-              repeatCount="none"
-              fill="freeze"
-            ></animate>
-          </stop>
+          <stop offset="0%" stopColor="#0096db" />
+          <stop offset="100%" stopColor="#004d40" />
         </linearGradient>
       </defs>
-      <path
-        stroke="#009688"
-        strokeWidth="1px"
-        strokeDasharray="2861px"
-        strokeDashoffset="-2861px"
-        d="M446,352h-.33q-4.68-37.26-25.73-55.6a73.82,73.82,0,0,0-14.19-9.55c-6.37,18.34-16.14,32-27.37,42.17q10.67,11.87,11.1,30.65c.2,2.43.31,5,.31,7.63v69.57q-1.06,21.12-11.63,31.41-11.76,11.43-36.6,11.43H261.83v-.09H210.09V339.21h64l51.38,115.5h16c9.2,0,16.37-1.63,19.17-4.35,2.15-2.09,3.65-7.36,4.06-14.18V405.64l-35.1-77.54Q388.5,309.82,388.49,236V173.22q0-52.93-28.42-77.44t-84-24.51H153.23V528.73H335.67q59.46,0,85.29-23.53t25.81-79.73V366.66A135.33,135.33,0,0,0,446,352ZM210.09,120.29h73.84q24.82,0,36.27,11.11t11.44,35.94v75.15q0,24.84-11.44,36.27T283.93,290.2H210.09Z"
+      {/* Outer circle with animation */}
+      <circle
+        cx="100"
+        cy="100"
+        r="80"
+        fill="none"
+        stroke="#0096db"
+        strokeWidth="5"
+        strokeDasharray="502.65"
+        strokeDashoffset="502.65"
       >
-        <animate id="p1" attributeName="stroke-dashoffset" values="2861;0" dur="3s" repeatCount="none" fill="freeze" />
+        <animate
+          attributeName="stroke-dashoffset"
+          from="502.65"
+          to="0"
+          dur="2s"
+          fill="freeze"
+        />
+      </circle>
+      {/* Letter O with animation */}
+      <path
+        d="
+          M100,40
+          a60,60 0 1,0 0,120
+          a60,60 0 1,0 0,-120
+        "
+        fill="none"
+        stroke="url(#gradient)"
+        strokeWidth="10"
+        strokeDasharray="377"
+        strokeDashoffset="377"
+      >
+        <animate
+          attributeName="stroke-dashoffset"
+          from="377"
+          to="0"
+          dur="2s"
+          begin="0.5s"
+          fill="freeze"
+        />
+      </path>
+      {/* Complete Letter M with middle "V" and animation */}
+      <path
+        d="
+          M70,140
+          L85,80
+          L100,110
+          L115,80
+          L130,140
+          L120,140
+          L100,100
+          L80,140
+          Z
+        "
+        fill="url(#gradient)"
+        stroke="url(#gradient)"
+        strokeWidth="1"
+        strokeDasharray="180"
+        strokeDashoffset="180"
+      >
+        <animate
+          attributeName="stroke-dashoffset"
+          from="180"
+          to="0"
+          dur="2s"
+          begin="1s"
+          fill="freeze"
+        />
+      </path>
+      {/* Star on top */}
+      <path
+        d="
+          M100 20 
+          L108 40 
+          L125 40 
+          L112 50 
+          L118 70 
+          L100 60 
+          L82 70 
+          L88 50 
+          L75 40 
+          L92 40 
+          Z
+        "
+        fill="#f3c518"
+        opacity="0"
+      >
+        <animate
+          attributeName="opacity"
+          from="0"
+          to="1"
+          dur="0.5s"
+          begin="2s"
+          fill="freeze"
+        />
       </path>
     </svg>
   );
